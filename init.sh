5a83238005680e73635304080f255d936bf2be3e
yumcyawiz <hashdolf1923@gmail.com>
2021-08-21 11:03:19 +0900

    activate nginx log

--- init.sh
+++ init.sh
@@ -7,8 +7,8 @@ make build
 if [ -f /var/lib/mysql/mysqld-slow.log ]; then
     sudo mv /var/lib/mysql/mysqld-slow.log /var/lib/mysql/mysqld-slow.log.$(date "+%Y%m%d_%H%M%S")
 fi
-if [ -f /var/log/nginx/access_log.tsv ]; then
-    sudo mv /var/log/nginx/access_log.tsv /var/log/nginx/access_log.tsv.$(date "+%Y%m%d_%H%M%S")
+if [ -f /var/log/nginx/access.log]; then
+    sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.$(date "+%Y%m%d_%H%M%S")
 fi
 
 cd /home/isucon
