9722ff8d40fc127e8683ca222f615fae931316a3
yumcyawiz <hashdolf1923@gmail.com>
2021-08-21 11:31:29 +0900

    apply slow query

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ set -ex
 cd /home/isucon/webapp/go
 make build
 
-if [ -f /var/lib/mysql/mysqld-slow.log ]; then
-    sudo mv /var/lib/mysql/mysqld-slow.log /var/lib/mysql/mysqld-slow.log.$(date "+%Y%m%d_%H%M%S")
+if [ -f /var/lib/mysql/slow.log ]; then
+    sudo mv /var/lib/mysql/slow.log /var/lib/mysql/slow.log.$(date "+%Y%m%d_%H%M%S")
 fi
 if [ -f /var/log/nginx/access.log ]; then
     sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.$(date "+%Y%m%d_%H%M%S")
